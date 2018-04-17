eab0fdbe39711b88f3d4ff58b8b34c6613d52c27
huangwei <huangwei1983@gmail.com>
2018-04-17 16:13:06 +0800

    在 Ubuntu 目标主机上自动安装 python-minimal，修正用户密码设置错误

--- ssh-copy-id/0_batch_ssh-copy-id.sh
+++ ssh-copy-id/0_batch_ssh-copy-id.sh
@@ -21,9 +21,21 @@ ports=(
 
 i=0
 
+install_python() {
+  user="${1}"
+  host="${2}"
+  port="${3}"
+  ret=$(ssh "${user}"@${host} -p ${port} 'if [[ $(uname -v) =~ .*Ubuntu.* ]];then echo ubuntu;fi')
+  if [[ $ret = 'ubuntu' ]];then
+    ssh "${user}"@${host} -p ${port} -t 'sudo apt update && sudo apt install python-minimal'
+  fi
+}
+
 for host in "${hosts[@]}";do
   user=${users[$i]}
-  ssh-copy-id -i "$HOME/.ssh/id_dsa.pub" "${user}"@${host} -p ${ports[$i]}
+  ssh-copy-id -i "$HOME/.ssh/id_rsa.pub" "${user}"@${host} -p ${ports[$i]}
+  # patch for Ubuntu 16.04 server without python-minimal installed as pre-requisite
+  install_python "${user}" "${host}" "${ports[$i]}"
   i=$((i+1))
 done
 
